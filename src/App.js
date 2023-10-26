478f1fa03cc437fe07702971b8eb38fd01284884
Egor <gtg194@mail.ru>
2023-10-26 13:04:36 +0300

    //

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Users from './components/users'
+import AddUser from './components/AddUser';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const baseUrl = "https://reqres.in/api/users?page=1"
+
+class App extends React.Component {
+  constructor(props){
+
+    axios.get(baseUrl).then((res) => {
+      this.setState({users: res.data.data})
+    })
+    super(props)
+    this.state = {
+        users: [
+    // {
+    //     id: 1,
+    //     firstName: "Ivan",
+    //     lastName: "Pirogov",
+    //     bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, mollitia.",
+    //     age: 33,
+    //     isHappy: true
+    // },
+    // {
+    //     id: 2,
+    //     firstName: "Petr",
+    //     lastName: "Dolin",
+    //     bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora aspernatur nam facilis tenetur?",
+    //     age: 22,
+    //     isHappy: false
+    // }
+]
+    }
+    this.addUser = this.addUser.bind(this)
+    this.deleteUser = this.deleteUser.bind(this)
+    this.editUsers = this.editUsers.bind(this)
+}
+  render() {
+    return (
+      <div>
+        <Header title="Список пользователей" />
+        <main>
+          <Users onEdit={this.editUsers} users={this.state.users} onDelete={this.deleteUser}/>
+        </main>
+        <aside>
+          <AddUser onAdd={this.addUser} />
+        </aside>
+      </div>
+    )
+  }
+  deleteUser(id){
+    this.setState({
+      users: this.state.users.filter((el)=>el.id !== id)
+    })
+  }
+  editUsers(user){
+    let allUsers = this.state.users
+    allUsers[user.id -1] = user
+
+    this.setState({users: []}, () => {
+      this.setState({users: [...allUsers]})
+    })
+  }
+  addUser(user){
+    const id = this.state.users.length + 1
+    this.setState({ users: [...this.state.users, {id, ...user}]})
+  }
 }
 
-export default App;
+export default App
